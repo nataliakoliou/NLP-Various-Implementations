5e54a593223d4cab3ac912a9a80963c3f760f868
nataliakoliou <83582927+nataliakoliou@users.noreply.github.com>
2023-03-25 17:31:43 +0200

    Update nlp-1b.py

--- Assignment-1/nlp-1b.py
+++ Assignment-1/nlp-1b.py
@@ -34,37 +34,19 @@ def replace_tokens(ngrams, vocab, min_freq, label):
 def create_vocab(train_ngrams, label):
     return {word for ngram in train_ngrams for word in ngram if word != label}
 
-def train(k, vocab, ngrams):
+def train(k, vocab, ngrams, n):
     ngram_model = {}
     ngram_counts = Counter(ngrams)
     words = sorted(set([word for ngram in ngrams for word in ngram]))
-    for ngram in ngrams:
-        curr_words = ngram[:-1]  # extracts the first n-1 words from the n-gram and assigns them to curr_words (sliding window)
+    for curr_words in product(words, repeat=n-1):
         total_count = sum([count for ngram, count in ngram_counts.items() if ngram[:-1] == curr_words])
-        for next_word in get_candidate_words(words, ngram_counts, curr_words):
+        for next_word in words:
             count = ngram_counts[curr_words + (next_word,)]
             ngram_model[curr_words + (next_word,)] = (count + k) / (total_count + k*len(vocab))
     return ngram_model
 
-def get_candidate_words(words, ngram_counts, curr_words):
-    candidate_words = set()
-    for next_word in words:
-        if ngram_counts.get(curr_words + (next_word,), 0) > 0:
-            candidate_words.add(next_word)
-    return candidate_words
-
 def evaluate(ngram_model, ngrams):
     total_log_prob = sum(math.log(ngram_model[ngram]) for ngram in ngrams)
-    perplexity = math.exp(-(total_log_prob/len(ngrams)))
-    return perplexity
-
-def evaluate(k, vocab, ngram_model, ngrams):
-    total_log_prob = 0
-    for ngram in ngrams:
-        if ngram in ngram_model:
-            total_log_prob += math.log(ngram_model[ngram])
-        else:
-            total_log_prob += k / (total_count + k*len(vocab))
     perplexity = math.exp(-(total_log_prob/len(ngrams)))
     return perplexity
 
@@ -89,24 +71,24 @@ def main():
     test_bigrams_0, test_bigrams_1, test_trigrams_0, test_trigrams_1 = preprocess(test_corpus, vocab_0, "<BOS>", "<EOS>", "<UNK>", False, 2), preprocess(test_corpus, vocab_1, "<BOS>", "<EOS>", "<UNK>", True, 2), preprocess(test_corpus, vocab_0, "<BOS>", "<EOS>", "<UNK>", False, 3), preprocess(test_corpus, vocab_1, "<BOS>", "<EOS>", "<UNK>", True, 3)
 
     # Bigram Model with k = 1 Smoothing, where 0: lowercase = False and 1: lowercase = True
-    bigram_model_a0, bigram_model_a1 = train(a, vocab_0, train_bigrams_0), train(a, vocab_1, train_bigrams_1)
+    bigram_model_a0, bigram_model_a1 = train(a, vocab_0, train_bigrams_0, 2), train(a, vocab_1, train_bigrams_1, 2)
     bi_perp_a0, bi_perp_a1 = evaluate(bigram_model_a0, test_bigrams_0), evaluate(bigram_model_a1, test_bigrams_1)
     print(bi_perp_a0)
 
     # Bigram Model with k = 0.01 Smoothing, where 0: lowercase = False and 1: lowercase = True
-    #bigram_model_b0, bigram_model_b1 = train(b, vocab_0, train_bigrams_0), train(b, vocab_1, train_bigrams_1)
+    #bigram_model_b0, bigram_model_b1 = train(b, vocab_0, train_bigrams_0, 2), train(b, vocab_1, train_bigrams_1, 2)
     #bi_perp_b0, bi_perp_b1 = evaluate(bigram_model_b0, test_bigrams_0), evaluate(bigram_model_b1, test_bigrams_1)
 
     # Trigram Model with k = 1 Smoothing, where 0: lowercase = False and 1: lowercase = True
-    #trigram_model_a0 = train(a, vocab_0, train_trigrams_0)
+    #trigram_model_a0 = train(a, vocab_0, train_trigrams_0, 3)
     #tri_perp_a0 = evaluate(trigram_model_a0, test_trigrams_0)
     #print(tri_perp_a0)
 
-    #trigram_model_a1 = train(a, vocab_1, train_trigrams_1)
+    #trigram_model_a1 = train(a, vocab_1, train_trigrams_1, 3)
     #tri_perp_a1 = evaluate(trigram_model_a1, test_trigrams_1)
 
     # Trigram Model with k = 0.01 Smoothing, where 0: lowercase = False and 1: lowercase = True
-    #trigram_model_b0, trigram_model_b1 = train(b, vocab_0, train_trigrams_0), train(b, vocab_1, train_trigrams_1)
+    #trigram_model_b0, trigram_model_b1 = train(b, vocab_0, train_trigrams_0, 3), train(b, vocab_1, train_trigrams_1, 3)
     #tri_perp_b0, tri_perp_b1 = evaluate(trigram_model_b0, test_trigrams_0), evaluate(trigram_model_b1, test_trigrams_1)
 
     #pretty_print(bi_perp_a0, bi_perp_a1,  bi_perp_b0, bi_perp_b1, tri_perp_a0, tri_perp_a1, tri_perp_b0, tri_perp_b1)
